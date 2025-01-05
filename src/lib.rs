a1a5cd8020c0713cb74e8efc9d3f97cf675b44e0
Maks Tamoian <maks.tamoian@gmail.com>
2025-01-06 00:19:16 +0100

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,92 @@
 use reqwest::blocking::Client as SyncClient;
 use reqwest::Client as AsyncClient;
-use mockito::{mock, Matcher};
-use std::error::Error;
+
 use serde_json::json;
-use tokio::runtime::Runtime;
+use std::error::Error;
 
 pub struct PushinatorClient {
     api_token: String,
+    base_url: String,
 }
 
 impl PushinatorClient {
     pub fn new(api_token: String) -> Self {
         PushinatorClient {
             api_token: api_token.to_string(),
+            base_url: "https://api.pushinator.com/".to_string(),
         }
     }
 
-    pub fn send_notification_sync(&self, channel_id: String, notification: &str) -> Result<(), Box<dyn Error>> {
-        let api_url = "https://api.pushinator.com/api/v1/send_notification";
-
-    let client = SyncClient::new();
-
-    let response = client
-        .post(api_url)
-        .header("Authorization", format!("Bearer {}", self.api_token))
-        .header("Content-Type", "application/json")
-        .header("User-Agent", "pushinator-rust/1.0")
-        .json(&json!({
-            "channel": channel_id,
-            "notification": notification
-        }))
-        .send()?;
-
-    if response.status().is_success() {
-        Ok(())
-    } else {
-        let status = response.status();
-        let error_body = response.text().unwrap();
-        Err(From::from(format!(
-            "Failed to send notification. Status: {}, Body: {}",
-            status,
-            error_body
-        )))
-    }
+    pub fn new_test(api_token: String, mock_url: String) -> Self {
+        PushinatorClient {
+            api_token: api_token.to_string(),
+            base_url: mock_url,
+        }
     }
 
-    pub async fn send_notification(&self, channel_id: String, notification: &str) -> Result<(), Box<dyn Error>> {
-        let api_url = "https://api.pushinator.com/api/v1/send_notification";
+    pub fn send_notification_sync(
+        &self,
+        channel_id: String,
+        notification: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        let api_url = format!("{}/api/v1/send_notification", self.base_url);
 
-    let client = AsyncClient::new();
+        let client = SyncClient::new();
 
-    let response = client
-        .post(api_url)
-        .header("Authorization", format!("Bearer {}", self.api_token))
-        .header("Content-Type", "application/json")
-        .header("User-Agent", "pushinator-rust/1.0")
-        .json(&json!({
-            "channel": channel_id,
-            "notification": notification
-        }))
-        .send()
-        .await?;
+        let response = client
+            .post(api_url)
+            .header("Authorization", format!("Bearer {}", self.api_token))
+            .header("Content-Type", "application/json")
+            .header("User-Agent", "pushinator-rust/1.0")
+            .json(&json!({
+                "channel": channel_id,
+                "notification": notification
+            }))
+            .send()?;
 
-    if response.status().is_success() {
-        Ok(())
-    } else {
-        let status = response.status();
-        let error_body = response.text().await?;
-        Err(From::from(format!(
-            "Failed to send notification. Status: {}, Body: {}",
-            status,
-            error_body
-        )))
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            let status = response.status();
+            let error_body = response.text().unwrap();
+            Err(From::from(format!(
+                "Failed to send notification. Status: {}, Body: {}",
+                status, error_body
+            )))
+        }
     }
+
+    pub async fn send_notification(
+        &self,
+        channel_id: String,
+        notification: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        let api_url = format!("{}/api/v1/send_notification", self.base_url);
+
+        let client = AsyncClient::new();
+
+        let response = client
+            .post(api_url)
+            .header("Authorization", format!("Bearer {}", self.api_token))
+            .header("Content-Type", "application/json")
+            .header("User-Agent", "pushinator-rust/1.0")
+            .json(&json!({
+                "channel": channel_id,
+                "notification": notification
+            }))
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            let status = response.status();
+            let error_body = response.text().await?;
+            Err(From::from(format!(
+                "Failed to send notification. Status: {}, Body: {}",
+                status, error_body
+            )))
+        }
     }
 }
 
@@ -80,6 +94,7 @@ impl PushinatorClient {
 mod tests {
     use super::*;
     use mockito::{mock, Matcher};
+    use tokio;
 
     #[test]
     fn test_send_notification_sync_success() {
@@ -94,8 +109,9 @@ mod tests {
             .with_status(200)
             .create();
 
-        let client = PushinatorClient::new("test-api-token".to_string());
-        let result = client.send_notification_sync("test-channel-id".to_string(), "Test notification");
+        let client = PushinatorClient::new_test("test-api-token".to_string(), mockito::server_url());
+        let result =
+            client.send_notification_sync("test-channel-id".to_string(), "Test notification");
 
         assert!(result.is_ok());
     }
@@ -114,8 +130,9 @@ mod tests {
             .with_body("{\"error\":\"Internal Server Error\"}")
             .create();
 
-        let client = PushinatorClient::new("test-api-token".to_string());
-        let result = client.send_notification_sync("test-channel-id".to_string(), "Test notification");
+        let client = PushinatorClient::new_test("test-api-token".to_string(), mockito::server_url());
+        let result =
+            client.send_notification_sync("test-channel-id".to_string(), "Test notification");
 
         assert!(result.is_err());
     }
@@ -133,9 +150,11 @@ mod tests {
             .with_status(200)
             .create();
 
-        let client = PushinatorClient::new("test-api-token".to_string());
-        let result = client.send_notification("test-channel-id".to_string(), "Test notification").await;
-
+        let client = PushinatorClient::new_test("test-api-token".to_string(), mockito::server_url());
+        let result = client
+            .send_notification("test-channel-id".to_string(), "Test notification")
+            .await;
+        println!("{:#?}", result);
         assert!(result.is_ok());
     }
 
@@ -153,8 +172,10 @@ mod tests {
             .with_body("{\"error\":\"Internal Server Error\"}")
             .create();
 
-        let client = PushinatorClient::new("test-api-token".to_string());
-        let result = client.send_notification("test-channel-id".to_string(), "Test notification").await;
+        let client = PushinatorClient::new_test("test-api-token".to_string(), mockito::server_url());
+        let result = client
+            .send_notification("test-channel-id".to_string(), "Test notification")
+            .await;
 
         assert!(result.is_err());
     }
